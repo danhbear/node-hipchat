a037ae98c45cdc96941060b896841fe7754966e1
Daniel Bear <daniel@hulu.com>
2012-05-28 13:59:34 -0700

    Read API key from dev directory. Change examples to be read-only.

--- example.js
+++ example.js
@@ -1,7 +1,18 @@
+var fs = require('fs');
+var apiKey = 'api-key-goes-here';
+try {
+  apiKey = fs.readFileSync('api_key', 'ascii').replace(/^\s+|\s+$/g, '');
+  console.log('Read api_key from disc:', apiKey);
+}
+catch (err) {
+  console.log('Failed to read api_key. Requests will probably fail.');
+}
+
 var chain = new (require('chainer'));
 var hipchat = require('./index');
-var hip = new hipchat('api-key-goes-here');
+var hip = new hipchat(apiKey);
 var rooms = hip.Rooms;
+var users = hip.Users;
 
 // Get a list of rooms.
 chain.add(function(){
@@ -21,20 +32,29 @@ chain.add(function(id){
   });
 });
 
-// Send a message to the room.
-chain.add(function(id){
-  rooms.message(id, 'Name', 'This is a test', function(err, res){
-    if (err) throw new Error(err.message);
-    console.log('Message sent: ', res);
-    chain.next(id);
-  });
-});
-
 // View history of a room.
 chain.add(function(id){
   rooms.history(id, function(err, res){
     if (err) throw new Error(err.message);
     console.log('Recent messages: ', res);
+    chain.next(id);
+  });
+});
+
+// Get a list of users.
+chain.add(function(){
+  users.list(function(err, res){
+    if (err) throw new Error(err.message);
+    console.log('Users: ', res);
+    chain.next(res.users[0].user_id);
+  });
+});
+
+// Get detailed user info.
+chain.add(function(id){
+  users.show(id, function(err, res){
+    if (err) throw new Error(err.message);
+    console.log('User #'+id+': ', res);
     chain.next(id);
   });
 });
